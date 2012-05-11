6eb564ca028f59a550fcd836e77b2057a5a769dd
Brodie Austin <brodie.austin@gmail.com>
2012-05-11 00:03:35 -0500

    made project page into floating grid with rows

--- index.php
+++ index.php
@@ -23,53 +23,55 @@
 	<div class="container">
 		<div class="row">
 			<div class="span8">
-				<div class="row">
-					<div class="span8">
-						<h1>Des Plaines History</h1>
-						<p>This collection brings together a wide range of materials related to local Des Plaines history. The bulk of these photographs, postcards, letters, and other items have been selected from the archives of the Des Plaines History Center. If you have any information about anything in the collection, feel free to leave your comments about particular items.</p>
-						<p>The following list of projects are a great way to explore related items in the Des Plaines History collection.</p>
-					</div>
-				</div>
+				<h1>Des Plaines History</h1>
+				<p>This collection brings together a wide range of materials related to local Des Plaines history. The bulk of these photographs, postcards, letters, and other items have been selected from the archives of the Des Plaines History Center. If you have any information about anything in the collection, feel free to leave your comments about particular items.</p>
+				<p>The following list of projects are a great way to explore related items in the Des Plaines History collection.</p>
+			</div>
+			<div class="span4">
+				<ul class="sm-links">
+					<li><a href="" class="sm-twitter"><span style="display: none;">Twitter</span></a></li>
+					<li><a href="" class="sm-tumblr"><span style="display: none;">Tumblr</span></a></li>
+					<li><a href="/cdm/viewfeed/collection/p15770coll1" class="sm-rss"><span style="display: none;">RSS</span></a></li>
+				</ul>
+			</div>
+		</div><!--first row-->
 				<?php
 		
 			$baseurl = '/cdm/search/collection/p15770coll1/searchterm/{param}/field/projec/mode/all/conn/and/order/title/ad/asc';
 				$projects = array(
 				'Browns Department Store' => array('description' => 'Description', 'param' => 'browns%20department%20store'),
-				'Camp Pine' => array('description' => 'Description', 'param' => 'camp%20pine'),
-				'Douglas Aircraft' => array('description' => 'Description', 'param' => 'douglas%20aircraft'),
-				'Maine Township Schools' => array('description' => 'Description', 'param' => 'maine%20township%20schools'),
-				'On the Streets of Des Plaines, 1915' => array('description' => 'Description', 'param' => 'on%20the%20streets%20of%20des%20plaines%2c%201915'),
-				'Plew Homes' => array('description' => 'Description', 'param' => 'plew%20homes'),
+				'Camp Pine' => array('description' => 'Hans Reinhold and Rudolph Velte were among the German P.O.W.s held at Camp Pine in Des Plaines, Illinois during World War II. These few letters and photographs offer insight into their experiences during their time as Camp Pine inmates through their return to Europe after the war.', 'param' => 'camp%20pine'),
+				'Douglas Aircraft' => array('description' => 'World War II era photographs and documents related to the Douglas Aircraft (McDonnell Douglas Corporation) factory in Des Plaines, Illinois. The factory site later became part of O’Hare International Airport.', 'param' => 'douglas%20aircraft'),
+				'Maine Township Schools' => array('description' => 'Photographs and postcards of schools built in Maine Township, Illinois from the mid-1870s through the mid-1930s. Includes public and parochial schools.', 'param' => 'maine%20township%20schools'),
+				'On the Streets of Des Plaines, 1915' => array('description' => 'Candid photographs taken on the streets of downtown Des Plaines, Illinois, circa 1915. Locations include the Congregational Church, Village Hall and the Chicago and Northwestern Railway station.', 'param' => 'on%20the%20streets%20of%20des%20plaines%2c%201915'),
+				'Plew Homes' => array('description' => 'Photographs from the archives of the W. L. Plew Company, a small real estate and construction company in Des Plaines, Illinois. Most of the homes pictured were built in the neighborhood known as Des Plaines Gardens during the 1920s and 1930s.', 'param' => 'plew%20homes'),
 				);
+			$i = 0;
+			$c = 0;
 			
 			foreach ($projects as $key => $project){
-				echo '<div class="row">';
-				echo '    <div class="span8"><hr />';
-				echo '        <div class="row">';
-				echo '            <div class="span5"><h3><a href="' . str_replace('{param}', $project['param'], $baseurl). '" title="' . $key . '">' . $key . '</a></h3><p>' . $project['description'] . '</p><p><a href="' . str_replace('{param}', $project['param'], $baseurl). '" title="' . $key . '" class="btn">View Project</a></div>';
-				echo '            <div class="span3"><a href="#" class="thumbnail"><img src="http://placehold.it/207x153"></a></div>';
-				echo '        </div>';
-				echo '    </div>';
-				echo '</div>';
+				$r = $i % 3;
+				
+				if ($r == 0){
+					echo '<hr /><div class="row">';
+					$c = 0;
+				}
+				
+				echo '	<div class="span4">';
+				echo '		<ul class="thumbnails"><li><a href="#" class="thumbnail"><img src="http://placehold.it/380x285"></a></li></ul>';
+				echo '		<h3><a href="' . str_replace('{param}', $project['param'], $baseurl). '" title="' . $key . '">' . $key . '</a></h3><p>' . $project['description'] . '</p><p><a href="' . str_replace('{param}', $project['param'], $baseurl). '" title="' . $key . '" class="btn">View Project</a></p>';
+				echo '	</div>';
+				
+				if ($c == 2){
+					echo '</div><!--end of row-->';
+				}
+				$i++;
+				$c++;
+				
 			}
 		?>
 			</div>
-			<div class="span4">
-				<div class="well sidebar-nav" style="background-color: #f7fdec;">
-					<p>Please explore our collections. You will find everything from pictures of old homes to street scenes in Des Plaines.</p>
-					<p>If you find something that you like, share it with the "Share" option above the image or leave a comment.</p>
-					<hr>
-					<address><a href="http://www.dppl.org/home/index.shtml" target="_blank" style="color: #63af00"><strong>Des Plaines Public Library</strong><br></a>1501 Ellinwood Street<br>Des Plaines, Illinois 60016<br>847.827.5551</address>
-					<address><a href="http://www.desplaineshistory.org/" target="_blank" style="color: #63af00"><strong>Des Plaines History Center</strong><br></a>781 Pearson Street<br>Des Plaines, Illinois 60016<br>847.391.5399</address>
-					<hr>
-					<ul class="sm-links">
-						<li><a href="" class="sm-twitter"><span style="display: none;">Twitter</span></a></li>
-						<li><a href="" class="sm-tumblr"><span style="display: none;">Tumblr</span></a></li>
-						<li><a href="/cdm/viewfeed/collection/p15770coll1" class="sm-rss"><span style="display: none;">RSS</span></a></li>
-					</ul>
-				</div>	
-			</div>
-		</div>	
-	</div>
+	
+	</div></!--container-->
 </body>
 </html>
