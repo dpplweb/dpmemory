6eee16c3ae4d571b2ce2ab67a642a2a5208b1f73
Brodie Austin <brodie.austin@gmail.com>
2013-04-02 13:28:20 -0500

    got dynamic version of map plugin working

--- scripts/jquery.maps.js
+++ scripts/jquery.maps.js
@@ -1,14 +1,45 @@
 $(function(){
-	if($('#metadata_locati').length != 0){
-	
-		var map_key = 'AIzaSyABEmDoSNEXi5uaPHLUeUU9AcGqhJbD5gw';
-		var loc = $.trim($('#metadata_locati').text());
-		var src = 'http://maps.googleapis.com/maps/api/staticmap?center={location}&zoom=16&size=441x200&markers=color:green%7C' + loc + '&sensor=false';
-		src = src.replace('{location}', loc);
+    
+    if($('#metadata_locati').length != 0){
+        var script = document.createElement("script");
+        script.type = "text/javascript";
+        script.src = "http://maps.googleapis.com/maps/api/js?key=AIzaSyABEmDoSNEXi5uaPHLUeUU9AcGqhJbD5gw&sensor=false&callback=initialize_map";
+        document.body.appendChild(script);
+        
+        var map_div = '<!--inserted h3 and div for map--><h3 id="details_accordion_map" class="accordion_header role="tab" aria-expanded="true">Map</h3><div class="accordion_window" role="tabpanel"><div id="map_canvas" style="width: 100%; height: 200px;"></div></div>';
 		
-		var mapDiv = '<h3 id="details_accordion_map" class="accordion_header accordion_header_closed ui-accordion-header ui-helper-reset ui-state-default ui-corner-all" role="tab" aria-expanded="false"><span class="accordion_header_icon accordion_header_icon_closed ui-icon ui-icon-triangle-1-e"></span><a tabindex="-1" href="javascript://;">Map</a></h3><div class="accordion_window" role="tabpanel"><div id="map_canvas"><img src="' + src + '" /></div>';
+		$('#details_accordion > .accordion_window').first().after(map_div).hide();
 		
-		$('#details_accordion > .accordion_window').first().after(mapDiv);
-		
-	}
+    }
+    
 });
+
+function initialize_map(){
+    var geocoder;
+    var loc = document.getElementById('metadata_locati').innerHTML.trim();
+    var latlng = new google.maps.LatLng(42.033889, -87.899722);
+    
+
+    var mapOptions = {
+	    zoom : 14,
+	    center : latlng,
+	    mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+
+    geocoder = new google.maps.Geocoder();
+    geocoder.geocode({ 'address': loc}, function(results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+	        mapOptions["center"] =  results[0].geometry.location;
+
+	        map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+
+	        var marker = new google.maps.Marker({
+		        map: map,
+		        position: results[0].geometry.location
+	        });
+	        
+	        document.getElementById("map_canvas").style.display = 'block';
+        }
+
+    });
+}
