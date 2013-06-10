9da7ee22ffda90483d39650410e13af3ef1d99b3
dpplweb <dppl.web@gmail.com>
2013-06-10 16:45:57 -0500

    updated dates for directories

--- collection-landing-pages/directories/index.php
+++ collection-landing-pages/directories/index.php
@@ -29,10 +29,10 @@
 		$has_images = True;
 		
 		$projects = array(
-			'Des Plaines and Rural City Directory' => array('description' => 'Residential and commercial telephone listings for Des Plaines, Illinois and surrounding area.', 'param' => 'des%20plaines%20and%20rural%20city%20directory', 'thumb' => 'dp-rural-city-directory-thumb', 'item_id' => '132'),
-			'Telephone Directory, Des Plaines, Illinois' => array('description' => 'Residential and commercial telephone directory of Des Plaines and Park Ridge, Illinois.', 'param' => 'telephone%20directory%2C%20des%20plaines%2C%20illinois', 'thumb' => 'telephone-directory-dp-thumb', 'item_id' => '229'),
-			'Telephone Directory, Norwood - Edison Park, Park Ridge and Des Plaines' => array('description' => 'Residential and commercial telephone directory of Des Plaines, Edison Park, Norwood Park and Park Ridge, Illinois.', 'param' => 'telephone%20directory%2C%20norwood%20-%20edison%20park%2C%20park%20ridge%20and%20des%20plaines', 'thumb' => 'telephone-directory-norwood-dp-thumb', 'item_id' => '521'),
-			'Telephone Directory, Des Plaines - Park Ridge' => array('description' => 'Residential and commercial telephone directory of Des Plaines and Park Ridge, Illinois.', 'param' => 'telephone%20directory%2C%20des%20plaines%20-%20park%20ridge', 'thumb' => 'telephone-directory-dp-park-ridge-thumb', 'item_id' => '1432'),
+			'Des Plaines and Rural City Directory' => array('description' => 'Residential and commercial telephone listings for Des Plaines, Illinois and surrounding area. 1940&ndash;1941.', 'param' => 'des%20plaines%20and%20rural%20city%20directory', 'thumb' => 'dp-rural-city-directory-thumb', 'item_id' => '132'),
+			'Telephone Directory, Des Plaines, Illinois' => array('description' => 'Residential and commercial telephone directory of Des Plaines and Park Ridge, Illinois. January 1941, December 1943, November 1949.', 'param' => 'telephone%20directory%2C%20des%20plaines%2C%20illinois', 'thumb' => 'telephone-directory-dp-thumb', 'item_id' => '229'),
+			'Telephone Directory, Norwood - Edison Park, Park Ridge and Des Plaines' => array('description' => 'Residential and commercial telephone directory of Des Plaines, Edison Park, Norwood Park and Park Ridge, Illinois. 1949.', 'param' => 'telephone%20directory%2C%20norwood%20-%20edison%20park%2C%20park%20ridge%20and%20des%20plaines', 'thumb' => 'telephone-directory-norwood-dp-thumb', 'item_id' => '521'),
+			'Telephone Directory, Des Plaines - Park Ridge' => array('description' => 'Residential and commercial telephone directory of Des Plaines and Park Ridge, Illinois. 1956&ndash;1960, 1962&ndash;1965.', 'param' => 'telephone%20directory%2C%20des%20plaines%20-%20park%20ridge', 'thumb' => 'telephone-directory-dp-park-ridge-thumb', 'item_id' => '1432'),
 			);
 		$i = 0;
 		$c = 0;
