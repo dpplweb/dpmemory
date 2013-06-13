0e14b4269fcba9f343eee25f06633eaf6cadad8d
Brodie Austin <brodie.austin@gmail.com>
2013-06-13 10:40:51 -0500

    added projects param to data

--- maps/data.php
+++ maps/data.php
@@ -1,8 +1,8 @@
 <?php
 
-    $project = $_GET['p'];
+    $projects = $_GET['p'];
 
-    $fh = file_get_contents('http://cdm15770.contentdm.oclc.org:81/dmwebservices/index.php?q=dmQuery/p15770coll1/0/title!locati/title/500/1/0/0/0/0/0/0/json');
+    $fh = file_get_contents('http://cdm15770.contentdm.oclc.org:81/dmwebservices/index.php?q=dmQuery/' . $projects . '/0/title!locati/title/500/1/0/0/0/0/0/0/json');
     
     echo $fh;
 ?>
